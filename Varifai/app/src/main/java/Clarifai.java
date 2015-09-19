b811edfbe8d9b3d4992db7d65aa509e83a7bca22
Izumi Konata <sasawat@outlook.com>
2015-09-19 17:43:33 -0400

    Ayyy. General framework of Varifai app works! Yay~

---
+++ Varifai/app/src/main/java/Clarifai.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Sasawat on 09/19/2015.
+ */
+public class Clarifai {
+
+    public double predict(String url, String concept)
+    {
+        return 0;
+    }
+
+    public void positive(String url, String concept)
+    {
+
+    }
+
+    public void negative(String url, String concept)
+    {
+
+    }
+
+    public void train(String concept)
+    {
+
+    }
+
+}
