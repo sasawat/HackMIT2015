93497d402c9971d6a430f4f879b875cc97a2876e
Izumi Konata <sasawat@outlook.com>
2015-09-19 16:20:58 -0400

    basic camera stuff works in ClarifaiTest

--- ClarifaiTest/app/src/main/java/com/bros/grandrapids/clarifaitest/ClarifaiTestActivity.java
+++ ClarifaiTest/app/src/main/java/com/bros/grandrapids/clarifaitest/ClarifaiTestActivity.java
@@ -1,16 +1,27 @@
 package com.bros.grandrapids.clarifaitest;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 public class ClarifaiTestActivity extends AppCompatActivity {
+
+    private ImageView testImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clarifai_test);
+        testImageView = (ImageView) findViewById(R.id.imageViewA);
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1111);
     }
 
     @Override
@@ -18,6 +29,13 @@ public class ClarifaiTestActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_clarifai_test, menu);
         return true;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1111) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            testImageView.setImageBitmap(thumbnail);
+        }
     }
 
     @Override
